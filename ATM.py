36c2018484a879f2ab84d40a1df6a30546d17a10
JM-Melon <ctw20.jakub.malecki@corketb.ie>
2024-09-16 19:25:28 +0100

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,32 @@
+# Date: Sept 2024
+# Author: MS Hayes
+# Purpose: This code displays an ATM menu
+print("\t|-------------------------------|")
+print("\t|\t LCCS BANK LIMITED\t|")
+print("\t|\t ATM Main Menu\t\t|")
+print("\t|\t\t\t\t|")
+print("\t|\t1. Balance Enquiry\t|")
+print("\t|\t2. Cash Lodgement\t|")
+print("\t|\t3. Cash Withdrawal\t|")
+print("\t|\t4. Cash Transfer\t|")
+print("\t|\t5. Change Language\t|")
+print("\t|\t6. Other Services\t|")
+print("\t|\t\t\t\t|")
+print("\t|\t7. Exit\t\t\t|")
+print("\t|-------------------------------|")
+print("\t|\t\t\t\t|")
+print("\t| CHOOSE AN OPTION >> \t\t|")
+print("\t|\t\t\t\t|")
+print("\t|-------------------------------|")
+print("")
+
+#traditional consoles menus use 6 or 8 buttons on the side of screen or the num pad to operate the menu while a touch screen GUI simply has the buttons on the screen and gives you the possibility to press them with your fingers, ussually resulting in less errors made.
+#1: displays the variable that represents your account balance.
+#2: allows you to add to your account balance by inserting cash into the ATM
+#3: Takes money out of your accounbt and returns the same amount in cash that is then given to you.
+#4: this allows you to transfer money to or from your account into another.
+#5: This changes language of your system
+#7: this leaves the program to allow another user to make use of itk
+
+
+
