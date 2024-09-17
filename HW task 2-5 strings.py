e249d8089b022ae5b0af78f3cccc129a193d35c2
JM-Melon <ctw20.jakub.malecki@corketb.ie>
2024-09-17 22:44:57 +0100

    Add files via upload

--- HW task 2-5 strings.py
+++ HW task 2-5 strings.py
@@ -1,47 +1,64 @@
 # Date: Sept 2024
 # Author: Jakub Malecki
-# Purpose: Hw task 2-5
+# Purpose: Hw task 2-8
+# 
+# 
+# #task 2
+# #print(pagram [:] would display the whole phrase index 0 - 43
+# 
+# #task 3
+# # it would not worka as its out of range as there is no index 4 in "Mary"
+# #you cannot change the value of a string after it is determined
+# 
+# #task 4
+# #i beleive the first one will work and display all 4 words
+# word1 = "Leaving"
+# word2 = "Cert"
+# word3 = "Computer"
+# word4 = "Science"
+# 
+# subjectName = word1 + word2 + word3 + word4
+# print(subjectName)
+# #no spaces id add a comma to each word
+# 
+# 
+# #i beleive that it will return the first 3 indexes and the  16th index onwards
+# pangram = "The quick brown fox jumps over the lazy dogs!"
+# print(pangram[:3] + pangram[16:])
+# # no space on the second word id change the 3 to a 4
+# 
+# 
+# #it will conjugate a noun into its plural states
+# 
+# noun = input("Enter a singular noun:")
+# print("The plural of ",noun," is ",noun,"s")
+# #u can use commas instead of the plusses
+# 
+# 
+# #task 5
+# # 1= d
+# # 2= 0123456789
+# # 3= Z0123456789
+# # 4= ABCDEFGHIJKLMONPWRSTUWXYZ0
+# # 5= abcdefghijklmnopqrstuvwxyz
+# # 6= b
+# # 7= f
+# # 8=ABCDEFGHIJKLMNOPQRSTUVWXYZ
+# # 9= A
 
+#task 6
+# I PREDICT THAT IT WILL PREFOMR THE SAME EQUATION 4 TIMES
 
-#task 2
-#print(pagram [:] would display the whole phrase index 0 - 43
+print("2 + %d = 4"  %2)
+print("2 + %d = %d"  %(2 , 4))
+print("%d + %d = %d"  %(2, 2, 4))
+print("%d + %d = %d"  %(2, 2, 2+2))
 
-#task 3
-# it would not worka as its out of range as there is no index 4 in "Mary"
-#you cannot change the value of a string after it is determined
+#TASK 7
 
-#task 4
-#i beleive the first one will work and display all 4 words
-word1 = "Leaving"
-word2 = "Cert"
-word3 = "Computer"
-word4 = "Science"
+# i think it will print( 3, 3.14 , 3 and Hi)
 
-subjectName = word1 + word2 + word3 + word4
-print(subjectName)
-#no spaces id add a comma to each word
+#task 8
+#it will print ( Hi . How are you )(msg Hal)
 
-
-#i beleive that it will return the first 3 indexes and the  16th index onwards
-pangram = "The quick brown fox jumps over the lazy dogs!"
-print(pangram[:3] + pangram[16:])
-# no space on the second word id change the 3 to a 4
-
-
-#it will conjugate a noun into its plural states
-
-noun = input("Enter a singular noun:")
-print("The plural of ",noun," is ",noun,"s")
-#it doesnt make all words plural 
-
-
-#task 5
-# 1= d
-# 2= 0123456789
-# 3= Z0123456789
-# 4= ABCDEFGHIJKLMONPWRSTUWXYZ0
-# 5= abcdefghijklmnopqrstuvwxyz
-# 6= b
-# 7= f
-# 8=ABCDEFGHIJKLMNOPQRSTUVWXYZ
-# 9= A
+#it would print RADIUS = (5, 2*math.pi*5))
