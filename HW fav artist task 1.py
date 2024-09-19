56eb8986dfb996d8981f75a70eeeaa5a4d12b8d3
JM-Melon <ctw20.jakub.malecki@corketb.ie>
2024-09-19 23:28:35 +0100

    Add files via upload

---
+++ HW fav artist task 1.py
@@ -0,0 +1,9 @@
+# Date: Sept 2024
+# Author: Jakub Malecki
+# Purpose: Hw task 1 p25
+
+
+favArtist = input("What is your favourite artist?:")
+
+compliment = " is brilliant!"
+print(favArtist + compliment)
