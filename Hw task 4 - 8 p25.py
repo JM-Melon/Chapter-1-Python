0a55e2402220dc95b97a062936a9760f1ed9267b
JM-Melon <ctw20.jakub.malecki@corketb.ie>
2024-09-22 23:40:27 +0100

    Add files via upload

---
+++ Hw task 4 - 8 p25.py
@@ -0,0 +1,69 @@
+# Date: Sept 2024
+# Author: Jakub Malecki
+# Purpose: Hw task 1 p25
+
+# #task 4
+# secs = (int(input("Enter a five digit amount of seconds:")))
+# mins = secs // 60 
+# print (("this is how many minutes that is :"),mins)
+
+# #task 5
+# keys = input("Enter any 5 keys seperately:")
+# a1 = ord(keys[0])
+# a2 = ord(keys[2])
+# a3 = ord(keys[4])
+# a4 = ord(keys[6])
+# a5 = ord(keys[8])
+# print(("these are your digits in ascii form:"), a1,a2,a3,a4,a5)
+ 
+# #task 6
+# units = (int(input("How many units of power do you use?:")))
+# pricePU = units * 0.19
+# amountDue = pricePU + 26.20
+# print("You are Due to pay",amountDue,"$")
+ 
+# task7
+
+# fish = 4.50
+# chips = 2.80
+# orderF = int(input("How much Fish would you like to order???:"))
+# orderC = int(input("How much Chips would you like to order???:"))
+# priceF = orderF * fish
+# priceC = orderC * chips
+# TPrice = priceF + priceC
+# vPrice = round(((TPrice/100)*109), 2)
+# print ("Your total is:", vPrice ,"$")
+# 
+
+
+# #task 8
+# userWord = input("Enter any 5 letter word (lower case & no spaces):")
+# key = int(5)
+# l1 = ord(userWord[0])
+# l2 = ord(userWord[1])
+# l3 = ord(userWord[2])
+# l4 = ord(userWord[3])
+# l5 = ord(userWord[4])
+# 
+# 
+# s1 = l1 + key 
+# s2 = l2 + key
+# s3 = l3 + key 
+# s4 = l4 + key
+# s5 = l5 + key
+# 
+# f1 = chr(s1)
+# f2 = chr(s2)
+# f3 = chr(s3)
+# f4 = chr(s4)
+# f5 = chr(s5)
+# 
+# print (("Your secret word becomes:"),f1+f2+f3+f4+f5)
+# 
+# 
+
+
+
+
+
+
