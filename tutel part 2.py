4c02d83e1d33ad4bf1c18a7f1258616d418ea8bc
JM-Melon <ctw20.jakub.malecki@corketb.ie>
2024-09-23 21:36:03 +0100

    Add files via upload

---
+++ tutel part 2.py
@@ -0,0 +1,61 @@
+# Date: Sept 2024
+# Author: Jakub Malecki
+# Purpose: learing tutel
+import turtle
+
+turtle.showturtle()
+turtle.shape("turtle")
+# 
+# for i in range (0,8):
+#     turtle.forward(120)
+#     turtle.right(45)
+# 
+# 
+# 
+# for i in range(0,4):
+#     turtle.forward(100)
+#     turtle.right(90)
+#     
+# turtle.right(45)
+# turtle.forward(75)
+# turtle.left(90)
+# turtle.forward(75)
+# 
+# for i in range (0,5):
+#     turtle.forward(100)
+#     turtle.right(144)
+
+turtle.speed(100)
+#     
+# for i in range (0,8):
+#     turtle.forward(100)
+#     turtle.right(45)
+#     
+# for i in range (0,1000):
+#     turtle.right(90)
+#     for i in range(0,8):
+#      turtle.forward(100)
+#      turtle.right(45)
+
+
+# 
+# turtle.right(22.5)
+# turtle.forward(100)
+
+for i in range (0,4):
+    turtle.left(120)
+    turtle.forward(100)
+turtle.left(22.5)
+turtle.forward(200)
+turtle.right(22.5)
+for i in range (0,4):
+    turtle.left(120)
+    turtle.forward(100)
+    
+turtle.left(82.5)
+turtle.forward(200)
+turtle.left(37.5)
+turtle.forward(100)
+turtle.left(142.5)
+turtle.forward(200)
+    
