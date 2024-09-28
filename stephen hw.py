be704ee1f2c7b3bb64fe035e2c791aafd532405f
JM-Melon <ctw20.jakub.malecki@corketb.ie>
2024-09-28 21:47:22 +0100

    Add files via upload

---
+++ stephen hw.py
@@ -0,0 +1,39 @@
+"""
+Jakub
+date:4/9/24
+task 1,2 p42
+"""
+
+# #task1
+# num1 = int(input("Enter your first number:"))
+# num2 = int(input("Enter your second number:"))
+# num3 = int(input("Enter your third number:"))
+# num4 = int(input("Enter your fourth number:"))
+# num5 = int(input("Enter your fifth number:"))
+# 
+# numList = [num1,num2,num3,num4,num5]
+# 
+# print(numList)
+# 
+# nums1 = num1 + 1
+# nums2 = num2 + 1
+# nums3 = num3 + 1
+# nums4 = num4 + 1
+# nums5 = num5 + 1
+# newlist = [nums1,nums2,nums3,nums4,nums5]
+# 
+# print(newlist)
+# 
+# #task2
+
+hours = [12,7,9,9,6,8,2]
+print("Stephen drinks", sum(hours)*0.5,"litres of milk in a week.")
+print("Stephen owes his dad", round((sum(hours)*0.5)*1.35,2),"euro per week.")
+
+
+
+
+
+
+
+
